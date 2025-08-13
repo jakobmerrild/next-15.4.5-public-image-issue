6b0e8772758838f40aea35591885fa077843a3c5
Jakob Merrild <jm@famly.co>
2025-08-13 13:52:13 +0200

    Added jpg image as well

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,10 @@ import Image from 'next/image';
 
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return <Image src="/us.png" alt="us flag" width={100} height={100} />;
+  return (
+    <div>
+      <Image src="/us.png" alt="us flag" width={100} height={100} />
+      <Image src="/potato.jpg" alt="potato" width={100} height={100} />
+    </div>
+  );
 }
