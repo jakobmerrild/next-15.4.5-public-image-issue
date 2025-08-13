354e7c0e9e423dbec7b49234f65df120944fb75b
Jakob Merrild <jm@famly.co>
2025-08-13 13:48:33 +0200

    Minimum example

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  return <Image src="/us.png" alt="us flag" width={100} height={100} />;
 }
